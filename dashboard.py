312c89d76a267219c07a70cba505dd9c86cbce5d
Ferdian Razak <143473200+ferdianrazak@users.noreply.github.com>
2024-02-27 02:07:41 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -79,8 +79,6 @@ daily_registered_rent_df = create_daily_registered_rent_df(main_df)
 season_rent_df = create_season_rent_df(main_df)
 monthly_rent_df = create_monthly_rent_df(main_df)
 weekday_rent_df = create_weekday_rent_df(main_df)
-workingday_rent_df = create_workingday_rent_df(main_df)
-holiday_rent_df = create_holiday_rent_df(main_df)
 weather_rent_df = create_weather_rent_df(main_df)
 
 
