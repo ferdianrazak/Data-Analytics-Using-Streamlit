7783f619a1af7cec86f3ad58827776e122d5d127
Ferdian Razak <143473200+ferdianrazak@users.noreply.github.com>
2024-02-27 04:45:38 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import streamlit as st
 
-df = pd.read_csv("dashboard/main_data.csv")
+df = pd.read_csv("./dashboard/main_data.csv")
 
 st.sidebar.title("Peneliti:")
 st.sidebar.markdown("**• Nama: Ferdian Razak**")
