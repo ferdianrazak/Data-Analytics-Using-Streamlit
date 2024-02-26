bc40b3b27b406873aad8f9922b58547e34943238
Ferdian Razak <143473200+ferdianrazak@users.noreply.github.com>
2024-02-27 03:12:51 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,24 +12,10 @@ st.sidebar.markdown(
 st.sidebar.markdown(
     "**• Dicoding: [ferdianrazak](https://www.dicoding.com/users/ferdianrazak/)**")
 
-def create_daily_rent_df(df):
-    daily_rent_df = df.groupby(by='Tanggal').agg({
-        'Total_Sewa': 'sum'
-    }).reset_index()
-    return daily_rent_df
-
-def create_daily_casual_rent_df(df):
-    daily_casual_rent_df = df.groupby(by='Tanggal').agg({'Non-member': 'sum'}).reset_index()
-    return daily_casual_rent_df
-
-def create_daily_registered_rent_df(df):
-    daily_registered_rent_df = df.groupby(by='Tanggal').agg({
-        'Member': 'sum'
-    }).reset_index()
-    return daily_registered_rent_df
-    
 def create_season_rent_df(df):
     season_rent_df = df.groupby(by='Musim')[['Member', 'Non-member']].sum().reset_index()
+    order_cuaca = ['Salju Ringan/Hujan', 'Berkabut/Berawan', 'Cerah/Sebagian Berawan']
+    monthly_rent_df = monthly_rent_df.reindex(order_cuaca, fill_value=0)
     return season_rent_df
 
 def create_monthly_rent_df(df, year):
@@ -40,12 +26,6 @@ def create_monthly_rent_df(df, year):
     ]
     monthly_rent_df = monthly_rent_df.reindex(order_bulan, fill_value=0)
     return monthly_rent_df
-
-def create_weekday_rent_df(df):
-    weekday_rent_df = df.groupby(by='Hari').agg({
-        'Total_Sewa': 'sum'
-    }).reset_index()
-    return weekday_rent_df
     
 def create_weather_rent_df(df):
     weather_rent_df = df.groupby(by='Cuaca')[['Member', 'Non-member']].sum().reset_index()
@@ -65,25 +45,15 @@ start_date, end_date = st.date_input(
 main_df = df [(df['Tanggal'] >= str(start_date)) & (df['Tanggal'] <= str(end_date))]
 
 # Menyiapkan berbagai dataframe
-daily_rent_df = create_daily_rent_df(main_df)
-daily_casual_rent_df = create_daily_casual_rent_df(main_df)
-daily_registered_rent_df = create_daily_registered_rent_df(main_df)
 season_rent_df = create_season_rent_df(main_df)
-weekday_rent_df = create_weekday_rent_df(main_df)
 weather_rent_df = create_weather_rent_df(main_df)
-
-# Membuat Dashboard secara lengkap
-
-# Membuat judul
-st.header('Final Project Data Analytics - Bike Sharing Dataset')
-
-# Membuat jumlah penyewaan bulanan
 monthly_rent_df_2011 = create_monthly_rent_df(df, 2011)
 monthly_rent_df_2012 = create_monthly_rent_df(df, 2012)
+
+# Membuat Dashboard secara lengkap
+st.header('Final Project Data Analytics - Bike Sharing Dataset')
 st.subheader('Tren jumlah pengguna per bulan pada 2011 dan 2012')
 fig, ax = plt.subplots(figsize=(24, 8))
-
-# Plot for 2011
 ax.plot(
     monthly_rent_df_2011.index,
     monthly_rent_df_2011['Total_Sewa'],
@@ -93,7 +63,6 @@ ax.plot(
     label='2011'  # Label for the legend
 )
 
-# Plot for 2012
 ax.plot(
     monthly_rent_df_2012.index,
     monthly_rent_df_2012['Total_Sewa'],
@@ -103,20 +72,14 @@ ax.plot(
     label='2012'  # Label for the legend
 )
 
-# Annotations for 2011
 for index, value in enumerate(monthly_rent_df_2011['Total_Sewa']):
     ax.text(index, value, str(value), ha='center', va='bottom', fontsize=12)
-
-# Annotations for 2012
-for index, value in enumerate(monthly_rent_df_2012['Total_Sewa']):
     ax.text(index, value, str(value), ha='center', va='bottom', fontsize=12)
-
-# Improve the formatting
 ax.tick_params(axis='x', labelsize=25, rotation=45)
 ax.tick_params(axis='y', labelsize=20)
-ax.legend()  # Display the legend
-
+ax.legend()
 st.pyplot(fig)
+
 # Membuah jumlah penyewaan berdasarkan kondisi cuaca
 st.subheader('Jumlah sewa berkaitan dengan cuaca')
 fig, ax = plt.subplots(figsize=(16, 8))
