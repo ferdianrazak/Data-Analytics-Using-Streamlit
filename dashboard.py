745b7f746b7a3f011cb4498b8477aa6f9405da89
Ferdian Razak <143473200+ferdianrazak@users.noreply.github.com>
2024-02-27 02:32:39 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import plotly.express as px
 import streamlit as st
 
 df = pd.read_csv("dashboard/main_data.csv")
